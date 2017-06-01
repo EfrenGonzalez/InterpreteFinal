1bbf4ebe3ce8eb569f3597e3ccb885b0bd722024
EfrenGonzalez <eduardoegg_97@hotmail.com>
2017-06-01 13:28:34 -0500

    Update AnalizadorSintactico.h

--- Clases/AnalizadorSintactico.h
+++ Clases/AnalizadorSintactico.h
@@ -1,26 +1,21 @@
 #ifndef ANALIZADORSINTACTICO_H
 #define ANALIZADORSINTACTICO_H
-#include <algorithm>
-#include <map>
 #include "Token.h"
-#include "Declaracion.h"
-#include "vector"
+#include <bits/stdc++.h>
 
 using namespace std;
 class AnalizadorSintactico
 {
     public:
         static map<string,Token> mapa;
-        map<string,Token> tokens;
         map<string,Token>::iterator it;
         AnalizadorSintactico();
         virtual ~AnalizadorSintactico();
-        void AnalizadorLexico();
-        void declarar();
+        vector<Token> VectorSintatico(string);
     protected:
 
     private:
-
+        void declarar();
 };
 
 #endif // ANALIZADORSINTACTICO_H
