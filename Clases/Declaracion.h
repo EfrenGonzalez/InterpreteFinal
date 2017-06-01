21d2b68445ee7716e135dd304531647546f4d2dd
EfrenGonzalez <eduardoegg_97@hotmail.com>
2017-06-01 16:09:59 -0500

    Update Declaracion.h

--- Clases/Declaracion.h
+++ Clases/Declaracion.h
@@ -1,5 +1,6 @@
 #ifndef DECLARACION_H
 #define DECLARACION_H
+#include <bits/stdc++.h>
 #include "Instruccion.h"
 #include "Token.h"
 
@@ -11,11 +12,13 @@ class Declaracion : public Instruccion
         Declaracion(Token,Token);
         void imprimir();
         void ejecutar();
-        Token identificador;
+        map<string,pair<int,string> >::iterator it;
+        bool evaluar(map<string,Token>&,map<string,pair<int,string> >&,vector<Token>&,int&);
+        Token variable;
         Token tipo;
     protected:
 
     private:
 };
 
-#endif // DECLARACION_H
+#endif
