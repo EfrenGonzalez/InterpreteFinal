78114f647c214f8b9da7b200dc25f928c8f3a259
EfrenGonzalez <eduardoegg_97@hotmail.com>
2017-06-01 16:09:05 -0500

    Update Declaracion.cpp

--- Clases/Declaracion.cpp
+++ Clases/Declaracion.cpp
@@ -1,9 +1,6 @@
-#include "string"
-#include "iostream"
 #include "Declaracion.h"
-#include <stdlib.h>
+#include <bits/stdc++.h>
 
-using namespace std;
 Declaracion::Declaracion()
 {
     //ctor
@@ -19,11 +16,21 @@ void Declaracion::imprimir()
 
 }
 
-void Declaracion::ejecutar(){
-
-}
-
-Declaracion::Declaracion(Token ti,Token ide){
-    this->tipo = ti;
-    identificador = ide;
+bool Declaracion::evaluar(map<string,Token>& mapa,map<string,pair<int,string> >& tV,vector<Token>& Tokens,int& voy)
+{
+    Tokens[voy]=tipo;
+    Tokens[voy+1]=variable;
+    if (Tokens[voy+2].Nombre==";")
+    {
+        if (variable.Valor==1)
+        {
+            it=tV.find(variable.Nombre);
+            if (it!=tV.end()) return false;
+            tV[variable.Nombre]=make_pair(variable.Valor,"");
+            voy+=3;
+            return true;
+        }
+        else return false;
+    }
+    else return false;
 }
