31841c3a12ced9e31e05340790fc5f5408f7685b
erickgtzh <0193662@up.edu.mx>
2017-06-01 15:38:31 -0500

    Update Declaracion.h

--- Clases/Declaracion.h
+++ Clases/Declaracion.h
@@ -1,18 +1,14 @@
 #ifndef DECLARACION_H
 #define DECLARACION_H
 #include "Instruccion.h"
-#include "map"
-#include "string"
 #include "Token.h"
 
-using namespace std;
-class Declaracion : Instruccion
+class Declaracion : public Instruccion
 {
     public:
         Declaracion();
         virtual ~Declaracion();
         Declaracion(Token,Token);
-        void TablaDeSimbolos(string,string);
         void imprimir();
         void ejecutar();
         Token identificador;
