c1be428a447c14349d3a71b46a59e553883b1e9d
erickgtzh <0193662@up.edu.mx>
2017-06-01 18:07:27 -0500

    Update Asignacion.cpp

--- Clases/Asignacion.cpp
+++ Clases/Asignacion.cpp
@@ -1,5 +1,8 @@
 #include "Asignacion.h"
+#include "iostream"
+#include <bits/stdc++.h>
 
+using namespace std;
 Asignacion::Asignacion()
 {
     //ctor
@@ -10,10 +13,38 @@ Asignacion::~Asignacion()
     //dtor
 }
 
-void Asignacion::ejecutar(){
-
+void Asignacion::asignar(pair<bool,double>& inst,map<string,pair<int,string> >& tV,vector<Token>& Tokens,int& voy)
+{
+    it=tV.find(variable.Nombre);
+    bool checar = true;
+    if(checar){
+        if(inst.first==false){
+            checar = false;
+            cout << "Error" << endl;
+        }
+        else{
+            if (tV[variable.Nombre].first==30) {
+                tV[variable.Nombre].second=static_cast<int>(inst.second);
+            }
+            else if(tV[variable.Nombre].first==31){ //float
+                tV[variable.Nombre].second=static_cast<float>(inst.second);
+            }
+            else if(tV[variable.Nombre].first==32){ //true
+                tV[variable.Nombre].second=static_cast<bool>(inst.second);
+            }
+            else if(tV[variable.Nombre].first==33){ //false
+                tV[variable.Nombre].second=static_cast<bool>(inst.second);
+            }
+            else{
+                cout << "Error, formato inválido" << endl;
+                checar = false;
+            }
+        }
+        cout << "Error" << endl;
+        checar = false;
+    }
 }
 
-void Asignacion::imprimir(){
+void Asignacion::ejecutar(){
 
 }
