b8a99bacbd21049f0f24e48f55698fa135b36009
erickgtzh <0193662@up.edu.mx>
2017-06-02 11:54:20 -0500

    Update Impresion.h

--- Clases/Impresion.h
+++ Clases/Impresion.h
@@ -2,13 +2,16 @@
 #define IMPRESION_H
 #include "Instruccion.h"
 #include "Expresion.h"
+#include <bits/stdc++.h>
 
-class Impresion : Instruccion
+class Impresion : public Instruccion
 {
     public:
         Impresion();
         virtual ~Impresion();
         void ejecutar();
+        void imprimir(map<string,Token>& mapa,map<string,Variable>& tV,vector<Token>& Tokens,int& voy);
+        map<string,Variable>::iterator it;
         Expresion exp;
     protected:
 
