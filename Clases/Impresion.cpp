1376ad2f980f7cfa282bfe24c5454d88254e1318
erickgtzh <0193662@up.edu.mx>
2017-06-02 11:53:41 -0500

    Update Impresion.cpp

--- Clases/Impresion.cpp
+++ Clases/Impresion.cpp
@@ -1,5 +1,8 @@
 #include "Impresion.h"
+#include "Expresion.h"
+#include <bits/stdc++.h>
 
+using namespace std;
 Impresion::Impresion()
 {
     //ctor
@@ -10,5 +13,26 @@ Impresion::~Impresion()
     //dtor
 }
 
-void Impresion::ejecutar(){
+void Impresion::ejecutar() {
+
+}
+
+void Impresion::imprimir(map<string,Token>& mapa,map<string,Variable>& tV,vector<Token>& Tokens,int& voy){
+    pair<bool,double> valor = exp.Evaluar(mapa,tV,Tokens,voy);
+    it = tV.find(variable.Nombre);
+    if (it != tV.end())
+    {
+        if (Tokens[voy]="impresion"  && Tokens[voy+2]=";")
+        {
+            it=tV.find(variable.Nombre);
+            if (it!=tV.end()) { cout << "Error en la Impresion" << endl; break; }
+            if (tipo.Valor==20) cout << variable.Nombre << " " << valor.second;
+            else if (tipo.Valor==21) cout << variable.Nombre << " " << valor.second;
+            else if (tipo.Valor==22) cout << variable.Nombre << " " << valor.second;
+            else if (tipo.Valor==23) cout << variable.Nombre << " " << valor.second;
+            voy+=3;
+        }
+    }else{
+        cout << "Error la variable no se encuentra" << endl;
+    }
 }
