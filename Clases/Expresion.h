ab77d05fa82223c5ce489c74976c21b1347d1f69
EfrenGonzalez <eduardoegg_97@hotmail.com>
2017-06-01 20:44:34 -0500

    Update Expresion.h

--- Clases/Expresion.h
+++ Clases/Expresion.h
@@ -1,15 +1,17 @@
 #ifndef EXPRESION_H
 #define EXPRESION_H
+#include <bits/stdc++.h>
+#include "Token.h"
+#include "Instruccion.h"
 
-
-class Expresion
+class Expresion : public Instruccion
 {
     public:
         Expresion();
         virtual ~Expresion();
-
+        vector<Token> tE;
+        pair<bool,double> Evaluar(map<string,Token>&,map<string,pair<int,string> >&,vector<Token>&,int&);
     protected:
-
     private:
 };
 
