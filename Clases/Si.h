4ec44168c5f4ee475fc2bfd2ddc8f55a757e6dac
erickgtzh <0193662@up.edu.mx>
2017-06-02 14:12:41 -0500

    Create Si.h

---
+++ Clases/Si.h
@@ -0,0 +1,19 @@
+#ifndef SI_H
+#define SI_H
+#include "Expresion.h"
+
+class Si
+{
+    public:
+        Si();
+        virtual ~Si();
+        void ejecutar();
+        void imprimir();
+        void ejecutarSi(map<string,Token>&,map<string,Variable>&,vector<Token>&,int&);
+        Expresion exp;
+    protected:
+
+    private:
+};
+
+#endif // SI_H
