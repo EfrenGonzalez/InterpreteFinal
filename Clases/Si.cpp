48cb389d07a013c841b514a2d09a1df246fa661c
erickgtzh <0193662@up.edu.mx>
2017-06-02 14:35:00 -0500

    Update Si.cpp

--- Clases/Si.cpp
+++ Clases/Si.cpp
@@ -10,17 +10,28 @@ Si::~Si()
     //dtor
 }
 
-void Si::ejecutarSi(map<string,Token>& mapa,map<string,Variable>& tV,vector<Token>& Tokens,int& voy)
+bool Si::ejecutarSi(map<string,Token>& mapa,map<string,Variable>& tV,vector<Token>& Tokens,int& voy)
 {
+    int aux = voy+1;
     vector<int> numTok;
     pair<bool,double> expR;
-    if(tV[Tokens[voy].Nombre].Tipo==41 && Tokens[voy+2].Nombre==";" && Tokens[voy+3].Nombre == "{"){
-        while(expR.first!=false){
-            if(Tokens[voy].Nombre!="}") {
-                numTok.push_back(tV[Tokens[voy].Nombre].Tipo);
-                expR = exp.Evaluar(mapa,tV,Tokens,voy);
-                voy++;
-            }else voy = voy+numTok.size();
+    pair<bool,double> condicion;
+    condicion = exp.Evaluar(mapa,tV,Tokens,aux);
+    if(condicion.first!=false) {
+        if(tV[Tokens[voy].Nombre].Tipo==41 && Tokens[voy+2].Nombre==";" && Tokens[voy+3].Nombre == "{") {
+            while(expR.first!=false){
+                if(Tokens[voy].Nombre!="}") {
+                    numTok.push_back(tV[Tokens[voy].Nombre].Tipo);
+                    expR = exp.Evaluar(mapa,tV,Tokens,voy);
+                    voy++;
+                }else voy = voy+numTok.size();
+            }
+            return true;
+        }
+        else{
+            cout << "Error con la condición del If" << endl;
+            return false;
         }
     }
+    else    return false;
 }
