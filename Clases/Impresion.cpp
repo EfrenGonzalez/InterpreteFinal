455d2f02115b2c76b6a9fea2cefa0e5f13c76557
erickgtzh <0193662@up.edu.mx>
2017-06-02 12:32:07 -0500

    Update Impresion.cpp

--- Clases/Impresion.cpp
+++ Clases/Impresion.cpp
@@ -17,22 +17,27 @@ void Impresion::ejecutar() {
 
 }
 
-void Impresion::imprimir(map<string,Token>& mapa,map<string,Variable>& tV,vector<Token>& Tokens,int& voy){
+void Impresion::imprimir(map<string,Token>& mapa,map<string,Variable>& tV,vector<Token>& Tokens,int& voy)
+{
+    Expresion exp;
     pair<bool,double> valor = exp.Evaluar(mapa,tV,Tokens,voy);
     it = tV.find(variable.Nombre);
     if (it != tV.end())
     {
-        if (Tokens[voy]="impresion"  && Tokens[voy+2]=";")
+        if (Tokens[voy].Nombre=="impresion"  && Tokens[voy+2].Nombre==";")
         {
             it=tV.find(variable.Nombre);
-            if (it!=tV.end()) { cout << "Error en la Impresion" << endl; break; }
+            if (it!=tV.end()) { cout << "Error en la Impresion" << endl; return; }
             if (tipo.Valor==20) cout << variable.Nombre << " " << valor.second;
             else if (tipo.Valor==21) cout << variable.Nombre << " " << valor.second;
             else if (tipo.Valor==22) cout << variable.Nombre << " " << valor.second;
             else if (tipo.Valor==23) cout << variable.Nombre << " " << valor.second;
-            voy+=3;
+            voy=voy+3;
         }
     }else{
         cout << "Error la variable no se encuentra" << endl;
     }
+}
+
+void Impresion::imprimir(){
 }
